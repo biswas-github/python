064882db6c0f964e901171d905ff48ca52e5317e
biswas-github <biswas.banstola1@gmail.com>
2024-10-03 08:07:08 +0545

    day 2

---
+++ dictonary.py
@@ -0,0 +1,5 @@
+# dictonary is key value types 
+address={"ram":'pokhara',"shyam":"lalitpur"}
+print(f"the type of address is {type(address)}")
+print(f"the data in the dictonary is {address}")
+print(address["ram"])
---
+++ qn1.py
@@ -0,0 +1,9 @@
+expences={
+    "rental":1,
+    "bed":5,
+    "sirak":5,
+    "food":8
+}
+total=sum(expences.values())
+print("the total of the expences is as \n")
+print(f"total= {total}")
