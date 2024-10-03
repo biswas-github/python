08aa63b345f81db0a2352da59204258ae44de14e
biswas-github <biswas.banstola1@gmail.com>
2024-10-03 08:20:27 +0545

    bill splitter

---
+++ moneysplit.py
@@ -0,0 +1,4 @@
+totalprice=int(input("enter the total amount in the bills : \t"))
+person=int(input("enter the no of people in the hotel : \t"))
+splitter=float(totalprice/person)
+print(f"the money one has to pay is : \t {splitter}")
